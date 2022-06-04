de4f543c169994d0f80582d023cd99daec866448
jokoul <74833985+jokoul@users.noreply.github.com>
2022-06-04 16:09:12 +0200

    make it better

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let photosArray = []; //we declare photosArray and initialize it with an empty a
 let count = 5; //For performance We start by retrieving only 5 image from the API then we will request for 30 more.
 const apiKey = "OnIt--EYalZb_qMpU2uNPvgp9mQUAuYzvAm3SY1b770"; //We use an environment
 
-let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Check if all images were loaded
 function imageLoaded() {
